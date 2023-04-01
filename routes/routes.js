08991824a4a7a330a186d8f9a275ac01dfccfaf6
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2023-04-01 20:50:11 +0300

    fixed.

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,9 @@
-import express from "express";
-import dotenv from "dotenv";
-import BotController from "../controllers/BotController.js";
+const express = require("express");
+const BotController = require("../controllers/BotController.js");
 const router = express.Router();
 const bot = new BotController();
 router.get("/", (req, res) => {
   bot.login(req, res);
 });
 
-export default router;
+module.exports = router;
